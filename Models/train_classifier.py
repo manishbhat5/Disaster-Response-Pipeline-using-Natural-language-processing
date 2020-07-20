a4fea5d201b2f53c82cf3be6fb447cc73548db2a
Mrinal Gupta <mgupta@usc.edu>
2020-07-19 20:41:10 -0700

    Add files via upload

---
+++ Models/train_classifier.py
@@ -0,0 +1,107 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+import nltk
+import re
+import numpy as np
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_multilabel_classification
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+from nltk.stem.porter import PorterStemmer
+import pickle
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+
+def load_data(database_filepath):
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql('Select * from Response', engine)
+    X = df['message']
+    Y = df.loc[:,'related':]
+
+    # Saving Column names
+    column_names = Y.columns
+
+    return X, Y, column_names
+
+
+def tokenize(text):
+    stop_words = stopwords.words("english")
+    lemmatizer = WordNetLemmatizer()
+    
+    # Normalization of text by removing all punctuation marks and converting the whole text to lower case
+    tokens = word_tokenize(re.sub(r"[^a-zA-Z0-9]", " ", text.lower()))
+    
+    # Lemmatization of text to stem words
+    clean_tokens = []
+    for tok in tokens:
+        if tok not in stop_words:
+            clean_tok = lemmatizer.lemmatize(tok)
+            clean_tok = lemmatizer.lemmatize(clean_tok, 'v')
+            clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+
+def build_model():
+    pipeline = Pipeline([('vect', CountVectorizer(tokenizer=tokenize)),
+                     ('tfidf', TfidfTransformer()),
+                     ('clf', MultiOutputClassifier(RandomForestClassifier()))])
+
+    parameters = {'clf__estimator__max_depth': np.arange(10,30,5), 'clf__estimator__n_estimators': np.arange(100,500,50)}
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+
+    return cv
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    Y_pred = pd.DataFrame(model.predict(X_test), columns=category_names, index=Y_test.index)
+    for i in range(len(category_names)):
+        print("Category Name: ", category_names[i])
+        print(classification_report(Y_test.iloc[:, i], Y_pred.iloc[:, i]))
+
+
+def save_model(model, model_filepath):
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
