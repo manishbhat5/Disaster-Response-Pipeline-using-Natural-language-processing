a8cc326b969c7e71356b0957c9a6999424166312
Mrinal Gupta <mgupta@usc.edu>
2020-07-20 18:13:22 -0700

    Added doc strings and comments to the code

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,66 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+
+    """
+    Read csv files of two dataframes and returns a concatenated dataframe
+    of both the datasets
+    
+    Input: messages_filepath: path of messages('disaster_messages.csv')
+        `` categories_filepath: path of categories('disaster_categories.csv')
+
+    Output: df: concatenated dataframe object of the two datasets, messages and categories
+    """
+
+    # read data from csv files
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    
+    # merge both the datasets and return the same
+    df = pd.merge(messages,categories,how = 'left', on='id')
+    return df
 
 
 def clean_data(df):
-    pass
 
+    """
+    Cleans the dataframe by splitting the category names from categories column
+    and drops the duplicates, if any.
+
+    Input: df: dataframe from load_data()
+    Output: df: clean dataframe df
+
+    """
+    
+    # split the categories column to extract the category names
+    categories = df['categories'].str.split(';',expand=True)
+    rows = categories.iloc[0]
+    columns = rows.apply(lambda x:x[:-2])
+
+    # set each value to be the last character of the string
+    categories.columns = columns
+    for column in categories:
+      categories[column] = categories[column].apply(lambda x: x[-1]).astype(int)
+
+    # drop duplicates for column 'id' 
+    df = pd.concat((df, categories), axis=1).drop('categories', axis=1).drop_duplicates(subset='id')
+    return df
 
 def save_data(df, database_filename):
-    pass  
+
+    """
+    Stores the dataset into a sqlite database
+
+    Input: df: clean disaster dataframe
+    database_filename: file name of database file
+
+    """
+
+    engine = create_engine('sqlite:///DisasterResponse.db')
+    df.to_sql('Response', engine, if_exists='replace', index=False)
+    engine.dispose()  
 
 
 def main():
